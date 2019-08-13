81d66e0c3ab5b47d4f1c4ee5b9c23972d1e9753c
Jitender Chand <jitender@edgecanvas.com>
2019-08-13 16:56:02 +0400

    message 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as Sentry from '@sentry/browser';
 import './App.css';
+
+Sentry.init({dsn: "https://61a77ee231ab491ab40cd264b170b567@sentry.io/1529511"});
+
+const check = () => {
+  throw Error('Error')
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={check}>click button</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
