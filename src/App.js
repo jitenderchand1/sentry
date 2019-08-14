385f20624ff36124a0a20d1dbdfd1f18e4b75827
Jitender Chand <jitender.chand1@tothenew.com>
2019-08-14 10:39:08 +0400

    m2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import * as Sentry from '@sentry/browser';
+import logo from './logo.svg';
 import './App.css';
-
-if(false){
-  Sentry.init({dsn: "https://f17512637ba248b595266674f5c43a55@sentry.io/1529718"});
-}
-
-
-
-Sentry.captureMessage("dsdd", "sadf")
 
 const check = () => {
   throw Error('new Error')
 }
+
+const check2 = () => {
+  throw Error('new Error')
+}
+
+const RELEASE = '0.0.1';
+
+Sentry.init({
+  dsn: 'https://0574b296fc16458b9067e2bd22ab23be@sentry.io/1530191',
+  release: RELEASE,
+  environment: 'prod'
+});
+
+Sentry.captureMessage("hello", "jitender")
 
 function App() {
   return (
@@ -21,6 +27,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={check}>click button</button>
+        <button onClick={check2}>click button 2</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
